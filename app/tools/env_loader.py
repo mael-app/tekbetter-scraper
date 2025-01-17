a400bc301a169cb3ac5a4573d3a27d6b5e790135
Eliot <eliot.amanieu@epitech.eu>
2025-01-17 08:53:54 +0100

    fix: env variable SCRAPER_CONFIG_FILE not required in public mode

--- app/tools/env_loader.py
+++ app/tools/env_loader.py
@@ -17,7 +17,7 @@ def check_env_variables():
     if os.getenv("SCRAPER_MODE") == "public" and not os.getenv("PUBLIC_SCRAPER_TOKEN"):
         log_error("Missing PUBLIC_SCRAPER_TOKEN environment variable")
         valid = False
-    if not os.getenv("SCRAPER_CONFIG_FILE"):
+    if not os.getenv("SCRAPER_CONFIG_FILE") and os.getenv("SCRAPER_MODE") == "private":
         log_error("Missing SCRAPER_CONFIG_FILE environment variable")
         valid = False
     else:
